b8c8d3f87221e8f833602413c16dd034730af7c1
schrobunlop <123216939+schrobunlop@users.noreply.github.com>
2024-08-15 23:47:21 -0600

    feat: init panel configuration

--- modules/home-manager/bundle/plasma-manager.nix
+++ modules/home-manager/bundle/plasma-manager.nix
@@ -3,6 +3,51 @@
 
   programs.plasma = {
     enable = true;
+    panels = [
+      {
+        location = "top";
+        floating = true;
+        height = 26;
+        widgets = [
+          {
+            kickoff = {
+              icon = "nix-snowflake-white";
+              sortAlphabetically = true;
+            };
+          }
+          "org.kde.plasma.panelspacer"
+          {
+            digitalClock = {
+              date = {
+                format.custom = "ddd, MMM dd • yyyy-MM-dd •";
+                position = "besideTime";
+              };
+            };
+          }
+          "org.kde.plasma.panelspacer"
+          "org.kde.plasma.systemtray"
+          "org.kde.plasma.showdesktop"
+        ];
+      }
+      {
+        location = "bottom";
+        lengthMode = "fit";
+        hiding = "autohide";
+        floating = true;
+        widgets = [
+          {
+            iconTasks = {
+              launchers = [
+                "applications:mullvad-vpn.desktop"
+                "applications:kitty.desktop"
+                "preferred://browser"
+                "applications:org.kde.dolphin.desktop"
+              ];
+            };
+          }
+        ];
+      }
+    ];
     input.keyboard.numlockOnStartup = "on";
     kscreenlocker = {
       lockOnResume = true;
