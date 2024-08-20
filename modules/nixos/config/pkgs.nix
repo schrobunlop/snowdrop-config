08c3e59dba5ed9720c4380ff7c20f963fbf445dc
schrobunlop <123216939+schrobunlop@users.noreply.github.com>
2024-08-19 23:20:22 -0600

    feat: add Blender with CUDA support

--- modules/nixos/config/pkgs.nix
+++ modules/nixos/config/pkgs.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
+    (blender.override { cudaSupport = true; })
     git
     godot_4
     kitty
--- modules/nixos/misc/unfree.nix
+++ modules/nixos/misc/unfree.nix
@@ -1,5 +1,9 @@
 { lib, ... }: {
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "blender"
+    "cuda_cccl"
+    "cuda_cudart"
+    "cuda_nvcc"
     "nvidia-x11"
     "nvidia-settings"
     "nvidia-persistenced"
